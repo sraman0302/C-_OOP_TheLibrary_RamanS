9a3188c542ee1d057440bb09f2bf8961f4eccb0f
Siddhant Raman <sraman0302@gmail.com>
2020-04-15 17:12:30 -0400

    Declared expected public members/functions

--- Book.h
+++ Book.h
@@ -10,6 +10,14 @@ class Book {
   int id;
 
  public:
+  int operator+(const Book& books);
+  void borrow_book();
+  void edit_book();
+  void remove_book();
+  void return_book();
+  void display();
+  void input();
+  void set_author();
 };
 
 #endif  // BOOK_H_INCLUDED
