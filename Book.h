e7ef69ca53ff97df9869961f7c95235761bce6df
Siddhant Raman <sraman0302@gmail.com>
2020-04-15 16:41:06 -0400

    Listed expected private variables

--- Book.h
+++ Book.h
@@ -1,6 +1,15 @@
 #ifndef BOOK_H_INCLUDED
 #define BOOK_H_INCLUDED
+#include "Member.h"
+class Book {
+ private:
+  string name;
+  string author;
+  string count;
+  vector<string> books;
+  int id;
 
+ public:
+};
 
-
-#endif // BOOK_H_INCLUDED
+#endif  // BOOK_H_INCLUDED
