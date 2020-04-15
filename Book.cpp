16dfa8da10d87a84da6c471e7ecc0913f4eddce2
Siddhant Raman <sraman0302@gmail.com>
2020-04-15 19:09:06 -0400

    Set up the classes members in .cpp file

---
+++ Book.cpp
@@ -0,0 +1,9 @@
+#include "Book.h"
+int operator+(const Book& books);
+void borrow_book();
+void edit_book();
+void remove_book();
+void return_book();
+void display();
+void input();
+void set_author();
